7244cd844858c2f7d1812e0d45e762d1a8a3f1f9
Le Cuong <dongthoigian31313@gmail.com>
2020-12-07 00:35:29 +0700

    fix image IoTs

--- routes/iot.js
+++ routes/iot.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 var IOT = require('../models/iot.model');
 var Model = require('../models/user.model');
+var AWS = require('aws-sdk');
+var station = require('../models/resources');
+
+let s3Bucket = new AWS.S3({
+    accessKeyId: "AKIAQUYJ6XTNNZWXTEFL",
+    secretAccessKey: "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",
+    region: "ap-southeast-1"
+  });
 
 /* GET home page. */
 router.get('/', async(req, res) => {
@@ -26,13 +34,35 @@ router.get("/:id", async(req, res) => {
 
 
 router.post("/", async(req, res) => {
-    try {
-        let rs = new IOT(req.body);
-        let result = await rs.save();
-        res.send(result);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+    const st = await station.findById(req.body.machine_id).exec();
+    buf = Buffer.from(req.body.filename.replace(/^data:image\/\w+;base64,/, ""),'base64')
+    const type = req.body.filename.split(';')[0].split('/')[1];
+    const data = {
+        Bucket: 'haiconmeo-static/cuong-image',
+        Key: `${st.name}`,
+        Body: buf,
+        ContentEncoding: 'base64',
+        ContentType: `image/${type}`
+    };
+    s3Bucket.putObject(data, (err, data) =>{
+        if (err) { 
+            console.log(err);
+            console.log('Error uploading data: ', data); 
+        } else {
+            console.log('successfully uploaded the image!', data.Location);
+        }
+    });
+    let rs = new IOT(req.body);
+    Object.assign(rs, { filename: `https://haiconmeo-static.s3-ap-southeast-1.amazonaws.com/cuong-image/${st.name}` })
+    let result = await rs.save();
+    res.send(result);
+    // try {
+    //     let rs = new IOT(req.body);
+    //     let result = await rs.save();
+    //     res.send(result);
+    // } catch (error) {
+    //     res.status(400).send(error);
+    // }
 })
 
 router.put("/:id", async(req, res) => {
